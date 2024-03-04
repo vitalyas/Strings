5825a9bd8b61d54ccbeed5cba8382920e1d5a047
vitaliy <vitalyas-zarochintsev@yandex.ru>
2024-03-05 02:54:18 +0700

    print string

---
+++ Strings/Strings/Strings.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::string str = "Hello World!";
+    int len = str.length();
+    char firstSym = str[0];
+    char lastSym = str[len - 1];
+
+    std::cout << "string: " << str << std::endl;
+    std::cout << "lenght: " << len << std::endl;
+    std::cout << "first symbol: " << firstSym << std::endl;
+    std::cout << "last symbol: " << lastSym << std::endl;
+}
